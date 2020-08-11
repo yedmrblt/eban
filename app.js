f4ba2153e01efe85724e4904260bfa5bc7407b98
Yunus Emre Demirbulut <y.emre.demirbulut@gmail.com>
2020-08-11 21:02:19 +0300

    POST/reset request added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const accounts = require('./data/accounts');
 
 // Routes
 const balanceRoutes = require('./routes/balance');
@@ -12,6 +13,11 @@ app.use('/event', eventRoutes);
 
 app.get('/', (req, res) => {
   res.send('EBANX Software Engineer Take-home assignment');
+});
+
+app.post('/reset', (req, res) => {
+  accounts = [];
+  res.status(200).send();
 });
 
 module.exports = app;
